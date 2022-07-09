f928b83008a95d490340e36bdeb533a5881febcf
unknown <pettersonandre15@gmail.com>
2022-07-08 22:59:02 -0300

    Novas funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Address from './components/Address/Address';
-import Information from './components/Information/Information';
 import Main from './components/Main/Main';
-import Team from './components/Team/Team';
 
 function App() {
   return (
     <div className="App">
       <Main />
-      <Team />
       <Address />
     </div>
   );
--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import './Address.css'
+import { useForm } from 'react-hook-form';
 
 function Address() {
+
+    const { register, handleSubmit, setValue } = useForm();
+
+    const onSubmit = (e) => {
+        console.log(e);
+    }
+
+    const checkCEP = (e) => {
+        const cep = e.target.value.replace(/\D/g, '');
+        console.log(cep);
+        fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {
+            console.log(data.uf);
+            setValue('address', data.logradouro);
+            setValue('city', data.localidade);
+            setValue('uf', data.uf);
+
+            if (data.uf == 'DF') {
+                alert("Que legal! Estamos disponiveis em " + data.localidade + ", marque já sua consulta.")
+            } else {
+                alert("Infelizmente ainda não estamos disponiveis " + (data.uf == 'RJ' ? " em " : "no ") + data.uf + " :(")
+            }
+        });
+
+
+    }
     return (
         <div className='local-area'>
             <div id="address">
                 <i className="bi bi-geo-alt-fill"></i>
                 <div id="address-details">
-                    <p>Rua Brasilia, 1733</p>
-                    <p>Bairro das Emas</p>
-                    <a href=""><button className='btn btn-primary'>Consultar Disponibilidade</button></a>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <label>
+                            CEP:
+                            <input type="text" {...register("cep")} onBlur={checkCEP} />
+                        </label>
+                        <button type="submit">Enviar</button>
+                    </form>
+
                 </div>
             </div>
             <div className="about-location">
